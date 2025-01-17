93ca70f97a9c03b259d691d39ed2d0af7245cead
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2025-01-17 17:10:54 +0530

    Built VideoBackground

---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,19 @@
+const VideoBackground = () => {
+  return (
+    <div>
+      <iframe
+        src={
+          "https://www.youtube.com/embed/NboIt4cIabw?si=9qzaDgdUYHxKRZE3" +
+          "?&autoplay=1&mute=1&vq=hd720"
+        }
+        className="w-screen aspect-video"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default VideoBackground;
