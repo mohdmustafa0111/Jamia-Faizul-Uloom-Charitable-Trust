395cfbb20af9d328656c234dfe24432b54cf174c
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2025-01-17 17:13:13 +0530

    Built VideoBackground

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      <div className="bg-[#fff6ef]">
+        <div className="container mx-auto px-4 py-8">
+          <h1 className="text-3xl font-bold mb-4">About</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default About;
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,13 @@
+import About from "./About";
 import Header from "./Header";
+import VideoBackground from "./VideoBackground";
 
 const Body = () => {
   return (
     <div>
       <Header />
+      <VideoBackground />
+      <About />
     </div>
   );
 };
